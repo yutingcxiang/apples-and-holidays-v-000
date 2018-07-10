a47e42c31880e6b5fc657eb920e86525e40abb0b
Christine Xiang <yutingcxiang@gmail.com>
2018-07-10 06:00:41 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,6 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
+    holiday.keys
     holiday.values.flatten
   end
 end
