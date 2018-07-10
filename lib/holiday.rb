98c574ac02710c2b9f8b413acc4356c6ba85b13c
Christine Xiang <yutingcxiang@gmail.com>
2018-07-10 06:43:47 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
         day.capitalize!
       end
       capitalized = cap_holidays.join(" ")
-      puts "#{capitalized}: #{supply.join(",")}"
+      puts "#{capitalized}: #{supply.join(", ")}"
       end
   end
 end
