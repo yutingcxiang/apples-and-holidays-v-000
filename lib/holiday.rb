53b141ba32f7dff61fa7fb36c0387c192ecd3b09
Christine Xiang <yutingcxiang@gmail.com>
2018-07-10 06:32:31 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,12 +57,12 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holidays|
+    puts season.to_s.capitalize! + ":"
     holidays.each do |holiday, supply|
       holiday.to_s.split(" ").capitalize!
       supply.each do |supply| 
         supply.capitalize!
       end
-      puts season.to_s.capitalize! + ":"
   end
 end
 
