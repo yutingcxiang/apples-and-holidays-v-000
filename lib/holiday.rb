a8365f6135aa48904691b68cd5aaf7b5d7368187
Christine Xiang <yutingcxiang@gmail.com>
2018-07-10 06:20:34 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,8 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supply|
-      str = holiday.to_s.capitalize!
-      puts str
+      puts season.to_s.capitalize!
     end
   end
 end
